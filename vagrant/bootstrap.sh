9d9d70ea4f2914271437e4456ef713219b288f6c
Lingkai Dong <lingkai.dong@arm.com>
2021-03-05 10:05:45 +0000

    Vagrant: support Mbed CLI 2
    To use Mbed CLI 2, the python package `mbed-tools` is required, and
    the version of CMake needs to be at least 3.19 so we fetch the latest
    from the offical PPA.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | \
+        gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+apt-add-repository -y 'deb https://apt.kitware.com/ubuntu/ focal main'
 apt-get update
 apt-get full-upgrade -y
 apt-get install -y \
