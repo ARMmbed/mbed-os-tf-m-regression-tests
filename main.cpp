42921a8cc3459782164d655fef75375da6392b8d
Lingkai Dong <lingkai.dong@arm.com>
2021-03-17 10:02:50 +0000

    TF-M IRQ API now removed for the IPC model
    The following commit in trusted-firmware-m
    
        b774fecd SPM: Remove *TFM* IRQ APIs for IPC Model
    
    means that we need to disable IRQ tests in the regression suite.

--- main.cpp
+++ main.cpp
@@ -26,16 +26,8 @@ int main(void)
     GREENTEA_SETUP(90, "default_auto");
 #endif
 
-    // Use TF-M regression test TIMER1 IRQ handler for the TIMER1 IRQ. The TF-M
-    // IRQ test requires its own handler to be installed.
-    NVIC_SetVector(TFM_TIMER1_IRQ, (uint32_t)TIMER1_Handler);
-
     tfm_log_printf("Starting TF-M regression tests\n");
 
-    // Disable deep sleep to avoid the TF-M IRQ test causing a hang, as the
-    // TF-M IRQ test currently uses an interrupt not necessarily capable of
-    // waking a target from deep sleep, depending on the target.
-    sleep_manager_lock_deep_sleep();
     uint32_t retval = tfm_non_secure_client_run_tests();
     TEST_ASSERT_EQUAL_UINT32(0, retval);
 
@@ -59,9 +51,6 @@ int main(void)
     tfm_log_printf("Waiting for Greentea host\n");
     GREENTEA_SETUP(90, "default_auto");
 #endif
-
-    // Disable deep sleep
-    sleep_manager_lock_deep_sleep();
 
     tfm_log_printf("Starting TF-M PSA API tests\r\n");
 
